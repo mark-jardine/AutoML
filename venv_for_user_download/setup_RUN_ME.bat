a9f18f8fe6bc011f4a336631090ecc8eb5658bdd
mark-jardine <77744624+mark-jardine@users.noreply.github.com>
2021-04-23 14:08:34 +0100

    Update setup_RUN_ME.bat

--- venv_for_user_download/setup_RUN_ME.bat
+++ venv_for_user_download/setup_RUN_ME.bat
@@ -1,8 +1,8 @@
-python3 -m pip install venv
-python3 -m venv venv
+!python3 -m pip install venv
+!python3 -m venv venv
 cd venv/
 source bin/activate
-python3 -m pip install -r ../requirements.txt
+!python3 -m pip install -r ../requirements.txt
 git clone https://github.com/mark-jardine/yolov5
 cd ..
 move myData.yaml venv/yolov5/data
